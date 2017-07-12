15372cb930a8e4474be904f07a3130fc6df56a50
Keijiro Takahashi <keijiro@gmail.com>
2017-07-13 00:51:43 +0900

    Added BiquadFilter to the tester.

--- Source/PortAudioTest.cpp
+++ Source/PortAudioTest.cpp
@@ -1,40 +1,26 @@
 #include <cstdio>
 #include <cmath>
 #include "portaudio.h"
+#include "BiquadFilter.h"
 
 namespace
 {
     unsigned int kSampleRate = 48000;
 
-    class Tester
-    {
-    public:
-
-        Tester() : time_(0) {}
-
-        float getNext()
-        {
-            time_ += 1.0f / kSampleRate;
-            return std::sinf(time_ * 440 * 2) * 0.5f;
-        }
-
-    private:
-        float time_;
-    };
-
     static int TesterCallback(
-        const void* /*inputBuffer*/, void* outputBuffer,
+        const void* inputBuffer, void* outputBuffer,
         unsigned long framesPerBuffer,
         const PaStreamCallbackTimeInfo* timeInfo,
         PaStreamCallbackFlags statusFlags,
         void* userData
     )
     {
-        auto tester = reinterpret_cast<Tester*>(userData);
+        auto filter = reinterpret_cast<BiquadFilter*>(userData);
+        auto in = reinterpret_cast<const float*>(inputBuffer);
         auto out = reinterpret_cast<float*>(outputBuffer);
 
         for (auto i = 0u; i < framesPerBuffer; i++)
-            *out++ = *out++ = tester->getNext();
+            *out++ = filter->feedSample(*in++);
 
         return 0;
     }
@@ -44,24 +30,26 @@ namespace
         auto err = Pa_Initialize();
         if (err != paNoError) return err;
 
-        Tester tester;
+        BiquadFilter filter;
+        filter.setLowpass(0.01f, 1);
+
         PaStream* stream;
 
         err = Pa_OpenDefaultStream(&stream,
-            0,          // no input channels
-            2,          // stereo output
+            1,          // no input channels
+            1,          // stereo output
             paFloat32,  // 32 bit floating point output
             kSampleRate,
             paFramesPerBufferUnspecified,
             TesterCallback,
-            &tester
+            &filter
         );
         if (err != paNoError) return err;
 
         err = Pa_StartStream(stream);
         if (err != paNoError) return err;
 
-        Pa_Sleep(4 * 1000);
+        Pa_Sleep(10 * 1000);
 
         err = Pa_StopStream(stream);
         if (err != paNoError) return err;
